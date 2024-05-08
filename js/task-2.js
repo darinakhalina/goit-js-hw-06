fdee49f8f94272fd6b8ca315f0dbd8afba080c65
Darina Khalina <darina.khalina@namecheap.com>
2024-05-08 12:50:10 +0300

    chore(hw6): add task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,30 @@
-console.log(2);
+class Storage {
+  #items;
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(item) {
+    this.#items.push(item);
+  }
+
+  removeItem(item) {
+    const i = this.#items.indexOf(item);
+
+    if (i !== -1) {
+      this.#items.splice(i, 1);
+    }
+  }
+}
+
+console.log('Task 2');
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
